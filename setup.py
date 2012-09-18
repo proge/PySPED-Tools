be0b9e454a5b0507ba81696d8970b57cdf1644d3
daniel-hartmann <daniel.hnn@gmail.com>
2012-09-18 11:12:15 -0300

    Created setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name = "PySPED-Tools",
+    packages = ["pysped_tools"],
+    version = "0.0.1",
+    description = "Common library for SPED specifications",
+    author = "Daniel Hartmann",
+    author_email = "daniel@proge.com.br",
+    url = "https://github.com/proge/PySPED-Tools",
+    download_url = "https://nodeload.github.com/proge/PySPED-Tools/tarball/v0.0.1",
+    keywords = ["sped", "brazil", "brasil"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        ],
+    long_description = """\
+PySPED Tools
+------------
+
+SPED is the Brazilian public system of digital bookkeeping.
+
+This module implements
+ - Signature for SSL requests
+ - Certificate manipulation
+"""
+)
